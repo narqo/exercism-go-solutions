598f2aeb928749bdd83e858098c36b94c0706076
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-08-24 02:23:08 +0300

    raindrops: re-read the task and KISS

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -11,33 +11,18 @@ var nToDrop = map[int]string{
 }
 
 func Convert(n int) string {
-	var (
-		s    string
-		prev int
-	)
-	for _, f := range primeFactors(n) {
-		if f == prev {
-			continue
-		}
-		if drop, ok := nToDrop[f]; ok {
-			s += drop
-			prev = f
-		}
+	var s string
+	if n%3 == 0 {
+		s += "Pling"
+	}
+	if n%5 == 0 {
+		s += "Plang"
+	}
+	if n%7 == 0 {
+		s += "Plong"
 	}
 	if len(s) == 0 {
 		return strconv.Itoa(n)
 	}
 	return s
-}
-
-func primeFactors(n int) []int {
-	factors := []int{}
-	d := 2
-	for n > 1 {
-		for ; n%d == 0; n /= d {
-			factors = append(factors, d)
-		}
-		d++
-	}
-	return factors
 }
