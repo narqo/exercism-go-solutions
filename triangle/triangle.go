caa7824b0c6845e2414d239c9c044cfa358b261d
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-08-25 02:30:14 +0300

    triangle: 1st try

---
+++ triangle/triangle.go
@@ -0,0 +1,47 @@
+package triangle
+
+import "math"
+
+const testVersion = 2
+
+// Kind represents a possible kind of the triangle
+type Kind uint8
+
+const (
+	NaT = iota // not a triangle
+	Equ        // equilateral
+	Iso        // isosceles
+	Sca        // scalene
+)
+
+// KindFromSides returns  if the triangle is equilateral, isosceles, or scalene.
+func KindFromSides(a, b, c float64) Kind {
+	if !isTriangle(a, b, c) {
+		return NaT
+	}
+	if a == b && b == c {
+		return Equ
+	}
+	if a == b || a == c || b == c {
+		return Iso
+	}
+	return Sca
+}
+
+func isTriangle(a, b, c float64) bool {
+	switch {
+	case a <= 0 || b <= 0 || c <= 0:
+		return false
+	case math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c):
+		return false
+	case math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0):
+		return false
+	case a+b < c:
+		return false
+	case a+c < b:
+		return false
+	case b+c < a:
+		return false
+	}
+	return true
+}
