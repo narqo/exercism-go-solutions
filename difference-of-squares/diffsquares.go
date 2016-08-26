8b05a381c57e4dcfde2abe8890065f20ae070c68
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-08-27 02:21:49 +0300

    difference-of-squares: simplify and optimize

--- difference-of-squares/diffsquares.go
+++ difference-of-squares/diffsquares.go
@@ -1,21 +1,19 @@
 package diffsquares
 
-import "math"
-
 func SquareOfSums(n int) int {
 	var s int
 	for i := 0; i <= n; i++ {
 		s += i
 	}
-	return int(math.Pow(float64(s), 2))
+	return s * s
 }
 
 func SumOfSquares(n int) int {
-	var s float64
-	for i := 0.; i <= float64(n); i++ {
-		s += math.Pow(i, 2)
+	var s int
+	for i := 0; i <= n; i++ {
+		s += i * i
 	}
-	return int(s)
+	return s
 }
 
 func Difference(n int) int {
