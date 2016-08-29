a3fcd41e8701846347804f3d22abc735309ca4fc
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-08-30 01:27:59 +0300

    house: refactoring to use tail-recursion

--- house/house.go
+++ house/house.go
@@ -2,17 +2,17 @@ package house
 
 import "strings"
 
+// Embed embeds a noun phrase as the object of relative clause with a transitive verb.
 func Embed(relPhrase, nounPhrase string) string {
 	return relPhrase + " " + nounPhrase
 }
 
+// Verse generates a verse of a song.
 func Verse(subject string, relPhrases []string, nounPhrase string) string {
 	if len(relPhrases) == 0 {
-		subject += Embed("", nounPhrase)
-	} else {
-		subject += " " + Embed(relPhrases[0], Verse("", relPhrases[1:], nounPhrase))
+		return Embed(subject, nounPhrase)
 	}
-	return strings.TrimSpace(subject)
+	return Verse(subject, relPhrases[:len(relPhrases)-1], Embed(relPhrases[len(relPhrases)-1], nounPhrase))
 }
 
 var phrases = []string{
@@ -30,6 +30,7 @@ var phrases = []string{
 	"the house that",
 }
 
+// Song generates the full text of "The House That Jack Built".
 func Song() string {
 	var s []string
 	for i := len(phrases) - 1; i >= 0; i-- {
