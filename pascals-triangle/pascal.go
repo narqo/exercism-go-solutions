6ab31b5b76b7f85533750060abe1390dbb31a272
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-08-30 02:27:00 +0300

    pascals-triangle: redundant var

--- pascals-triangle/pascal.go
+++ pascals-triangle/pascal.go
@@ -3,18 +3,16 @@ package pascal
 // Triangle computes Pascal's triangle up to a given number of rows.
 func Triangle(n int) [][]int {
 	t := make([][]int, n)
-	ncols := 1
 	for row := 0; row < n; row++ {
-		cols := make([]int, ncols)
+		cols := make([]int, row+1)
 
 		t[row] = cols
 		cols[0] = 1
 		cols[len(cols)-1] = 1
 
-		for col := 1; col < ncols-1; col++ {
+		for col := 1; col < len(cols)-1; col++ {
 			cols[col] = t[row-1][col-1] + t[row-1][col]
 		}
-		ncols++
 	}
 	return t
 }
