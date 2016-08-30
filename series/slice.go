d9fd377386ff6286ed71373ad880ad03e3c3c07d
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-08-30 23:38:36 +0300

    series: silly

---
+++ series/slice.go
@@ -0,0 +1,25 @@
+package slice
+
+// All returns a list of all substrings of s with length n.
+func All(n int, s string) []string {
+	var l []string
+	if n > len(s) {
+		return l
+	}
+	for i := 0; i < len(s) && i+n <= len(s); i++ {
+		l = append(l, s[i:i+n])
+	}
+	return l
+}
+
+// UnsafeFirst returns the first substring of s with length n.
+func UnsafeFirst(n int, s string) string {
+	l := All(n, s)
+	if len(l) > 0 {
+		return l[0]
+	}
+	return ""
+}
+
+//func First(int, string) (first string, ok bool) {
+//}
